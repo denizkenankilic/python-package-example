55922bf6e77a3b71f6d3bbf1077a43de442c3a06
Deniz Kenan Kilic <deniz.kenan.kilic@gmail.com>
2021-06-15 18:12:12 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="python-package-example",
-    version="0.0.1",
-    author="Deniz Kenan Kılıç",
-    author_email="example@domain.com",
-    description="Example package deployment of python packaging",
+    name="python-package-example", # Edit
+    version="0.0.1", # You can set the version here
+    author="Deniz Kenan Kılıç", # Edit
+    author_email="example@domain.com", # Edit
+    description="Example package deployment of python packaging", # Edit
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/denizkenankilic/python-package-example",
+    url="https://github.com/denizkenankilic/python-package-example", # Edit
     project_urls={
-        "Bug Tracker": "https://github.com/denizkenankilic/python-package-example/-/issues",
+        "Bug Tracker": "https://github.com/denizkenankilic/python-package-example/-/issues", # Edit
     },
     classifiers=[
         "Programming Language :: Python :: 3",
