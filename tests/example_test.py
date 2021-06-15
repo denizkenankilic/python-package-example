4ce581c3dca8185ba5fe9f0ae3beda616a138345
Deniz Kenan Kilic <deniz.kenan.kilic@gmail.com>
2021-06-15 18:45:52 +0300

    Update example_test.py

--- tests/example_test.py
+++ tests/example_test.py
@@ -1,7 +1,7 @@
 import unittest
 from unittest import TestCase
 
-from src.example_folder_1 import example
+from src.example_folder_1.example_folder_2 import example
 
 class TestMethods(TestCase):
     def test_example(self):
